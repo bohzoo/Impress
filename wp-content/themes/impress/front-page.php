17b587849859e4f0a8e14d5505d8d47ce00a25c5
bohzoo <yukigunibohzoo1126@gmail.com>
2019-03-31 22:31:48 +0900

    modify common style css

--- wp-content/themes/impress/front-page.php
+++ wp-content/themes/impress/front-page.php
@@ -5,5 +5,18 @@
   </div>
 </div>
 
+<section id="top-about-container" class="section">
+  <div class="sec-box">
+    <div class="content-width">
+      <div class="sec-ttl-box">
+        <h2 class="sec-ttl"><span class="en">ABOUT</span></h2>
+        <div class="sec-sub-ttl"><span class="ja">私たちについて</span></div>
+      </div>
+      <div id="top-about-box">
+      </div>
+    </div>
+  </div>
+</section>
+
 
 <?php get_footer(); ?>
